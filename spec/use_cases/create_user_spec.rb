d4e2deee5d1c08593e3e21e84bbae5b06c9a3940
Philip Gheno <minijag@gmail.com>
2014-05-05 14:41:12 -0500

    Started making create_user_spec.rb but didnt finish, about to merge

---
+++ spec/use_cases/create_user_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe HMMC::CreateUser do
+  let(:db) {HMMC.db}
+  before do
+    db.clear_everything
+    @user = db.create_user(:name => "Philip", :email => "this@that.com", :password => "makerL7")
+
+  end
+
+  context "error" do
+    it "ensures school id is valid" do
+
+      result = described_class.run(school_id: "")
+
+      expect(result.success?).to eq(false)
+      expect(result.error).to eq(:school_doesnt_exist)
+
+    end
+  end
+
+  context "success" do
+    it "ensures it returns miles and school" do
+      result = described_class.run(school_id: 1)
+      expect(result.success?).to eq(true)
+      expect(result.miles).to eq(10)
+
+      expect(result.school.name).to eq("Kempner HighSchool")
+    end
+  end
+end
