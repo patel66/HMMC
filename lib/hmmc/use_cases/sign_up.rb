d1d813baaf897c9b494e1f714f9bd329e7e51da2
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-05-07 18:43:50 -0500

    added invalid password for signup

--- lib/hmmc/use_cases/sign_up.rb
+++ lib/hmmc/use_cases/sign_up.rb
@@ -7,36 +7,12 @@ module HMMC
 
       check_if_email_taken = HMMC.db.get_user_by_email(email)
       return failure :invalid_name if name.empty?
+      return failure :invalid_password if password.empty?
       return failure :email_already_in_use if check_if_email_taken != nil
       new_user = HMMC.db.create_user(:name=> name, :email=> email, :password => password)
-      session = HMMC.db.create_session(:user_id => new_user.id)
-      success(:user => new_user, :session => session )
+      success(:user => new_user)
     end
-
-
-
   end
 end
 
 
-# user = HMMC.db.get_user(data[:email])
-#       if user != nil
-#         return failure :email_taken
-#       else
-#         user = HMMC.user = db.create_user(:name => "John", :email=> "John@mail.com", :password => "123")
-#         session = HMMC.db.create_session(user.id)
-#         return success :session_id => session.id
-#       end
-
-
-# controller
-
-# def SignUp
-## SignUp
-#    SignUp()   //
-#    CreateSchool()
-#    view sign in
-
-# # end
-
-#   SignOut
--- spec/use_cases/sign_up_spec.rb
+++ spec/use_cases/sign_up_spec.rb
@@ -1,26 +1,4 @@
-
 require "spec_helper"
-
-# module HMMC
-#   describe HMMC::SignUp do
-#     xit "returns an error if the email is already taken" do
-#       user1 = HMMC.db.create_user(:name => "John", :email=> "John@mail.com", :password => "123")
-#       result = SignUp.run({ :name => "John", :email=> "John@mail.com", :password => "123" })
-#       expect(result.success?).to eq(false)
-#       expect(result.error).to eq(:email_taken)
-#     end
-
-#     xit "creates the user and signs them in" do
-#       result = SignUp.run({ :name => "John", :email=> "John@mail.com", :password => "123" })
-#       expect(result.success?).to eq(true)
-#       expect(result.session_id).to be_a(Fixnum)
-#     end
-
-
-#   end
-# end
-
-
 
 describe HMMC::SignUp do
   let(:db){HMMC.db}
@@ -40,7 +18,7 @@ describe HMMC::SignUp do
   it "Can't sign up, if email is already in use" do
 
     result = described_class.run(:name=> "John", :email=> "John@mail.com", :password=> "1234")
-    binding.pry
+
     expect(result.success?).to eq true
 
     result2 = described_class.run(:name=> "Phil", :email=> "John@mail.com", :password=> "1234")
@@ -49,4 +27,12 @@ describe HMMC::SignUp do
     #expect(result.user.name).to eq "John"
 
   end
+
+  it "throws an error if the password is empty" do
+    result = described_class.run(:name=> "John", :email=> "John@mail.com", :password=> "")
+    expect(result.success?).to eq false
+    expect(result.error).to eq :invalid_password
+
+  end
+
 end
