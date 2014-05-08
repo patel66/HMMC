34af572245f2261546de5ec0b9b16272e0eff9e7
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-05-08 14:15:45 -0500

    working on user sign up

--- web/app/controllers/schools_controller.rb
+++ web/app/controllers/schools_controller.rb
@@ -10,9 +10,12 @@ class SchoolsController < ApplicationController
     params[:city]
     params[:street]
 
-    result = HMMC::CreateSchool.run(school_params)
+    # result = HMMC::CreateSchool.run(school_params)
+    # @school = result.school
+    # # /schools/:id(.:format)
+    # redirect_to "/schools/#{@school.id}"
+    result = HMMC::SignUp.run(:name => params[:user][:name], :email => params[:user][:email], :password=> params[:user][:password], :school_name => params[:school][:name], :street => params[:school][:street], :city => params[:school][:city], :students=> params[:school][:students])
     @school = result.school
-    # /schools/:id(.:format)
     redirect_to "/schools/#{@school.id}"
   end
 
@@ -26,7 +29,24 @@ class SchoolsController < ApplicationController
     params.require(:school).permit(:name, :state, :street, :city)
   end
 
+
 end
+
+# def signin
+# session_result = HMMC::SignIn(stuff)
+# session_id = session_result.session_id
+# session[:session_id] = session_id
+# end
+
+# def create_activity
+# session_id = session[:session_id]
+# user = HMMC::Database::InMemory.db.get_user_by_session(session_id)
+# school = HMMC::Database::InMemory.db.get_school_by_user(user.id)
+# # create_activity_result = HMMC::CreateActivity.run(school_id: school.id)
+# create_activity_result = HMMC::CreateActivity.run(school_id: params[:school_id])
+# end
+
+
    # result = RabbitDice::CreateGame.run(players: params[:players])
    #  error = result.error
    #  @game = result.game
