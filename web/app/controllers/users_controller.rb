3bff59ca45470b611acc3fed4521b729e83772e6
patel66 <patel66@sbcglobal.net>
2014-05-09 13:26:00 -0500

    changed layout of sign up

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
   def new 
 
   end
@@ -7,13 +6,4 @@ class UsersController < ApplicationController
   def login
 
   end 
-=======
-
-  def new
-
-  end
-
-
->>>>>>> c976a22d6c0f50b62c9c674b61ed4167f4546401
-
 end
